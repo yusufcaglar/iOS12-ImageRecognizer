51c99fec56db6db94042bed501367229d2694689
Yusuf ÇAĞLAR <y.caglar17@icloud.com>
2018-10-29 14:32:27 +0300

    Initial Commit

---
+++ Intelligent Image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intelligent Image
+//
+//  Created by Yusuf ÇAĞLAR on 29/10/2018.
+//  Copyright © 2018 Yusuf ÇAĞLAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
